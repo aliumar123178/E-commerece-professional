adadda83dd3c879761ff8b144ab20c1803311729
Ali Umar <au7800583@gmail.com>
2026-05-28 04:56:47 -0700

    fix vite config for netlify build

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api':{
+  server: {
+    proxy: {
+      '/api': {
         target: 'http://localhost:3000'
       },
-      '/images':{
+      '/images': {
         target: 'http://localhost:3000'
       }
     }
-  },
-  build: {
-    outDir: '../ecommerce-backend/dist'
   }
 })
