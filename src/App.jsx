30617d4adb0e98487d61bc7a0202457899140398
Ali Umar <au7800583@gmail.com>
2026-05-28 22:51:20 -0700

    Fixed frontend connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,75 @@
 import axios from "axios";
-import { HomePage } from "./pages/home/HomePage";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router";
+
+import { HomePage } from "./pages/home/HomePage";
 import { CheckoutPage } from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from "./pages/orders/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
 import { ErrorPage } from "./pages/ErrorPage";
+
 import "./App.css";
-import { useEffect,useState } from "react";
+
+// BACKEND URL
+const BASE_URL = "https://back-ecomerece-3.onrender.com";
 
 function App() {
   const [cart, setCart] = useState([]);
-  const loadCart = async ()=>{
-     const response = await axios.get("/api/cart-items?expand=product");
+
+  // LOAD CART
+  const loadCart = async () => {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}/api/cart-items?expand=product`
+      );
+
       setCart(response.data);
-    
+    } catch (error) {
+      console.error("Error loading cart:", error);
     }
+  };
+
+  // RUN ON PAGE LOAD
   useEffect(() => {
-    
     loadCart();
-    
   }, []);
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart} loadCart ={loadCart}/>} />
-      <Route path="checkout" element={<CheckoutPage cart={cart} loadCart= {loadCart}/>} />
-      <Route path="orders" element={<OrdersPage cart={cart} />} />
-      <Route path="tracking/:orderId/:productId" element={<TrackingPage cart={cart} />} />
-      <Route path="*" element={<ErrorPage />} />
+      <Route
+        index
+        element={
+          <HomePage
+            cart={cart}
+            loadCart={loadCart}
+          />
+        }
+      />
+
+      <Route
+        path="checkout"
+        element={
+          <CheckoutPage
+            cart={cart}
+            loadCart={loadCart}
+          />
+        }
+      />
+
+      <Route
+        path="orders"
+        element={<OrdersPage cart={cart} />}
+      />
+
+      <Route
+        path="tracking/:orderId/:productId"
+        element={<TrackingPage cart={cart} />}
+      />
+
+      <Route
+        path="*"
+        element={<ErrorPage />}
+      />
     </Routes>
   );
 }
